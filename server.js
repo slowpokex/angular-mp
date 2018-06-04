06083368c1737f8f1d814e0096505d85536c467d
Mikita Isakau <harley26.gold@gmail.com>
2018-06-04 22:56:12 +0300

    Add ability deploying to Heroku(Part 2)

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname + '/dist/angular-mp/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
