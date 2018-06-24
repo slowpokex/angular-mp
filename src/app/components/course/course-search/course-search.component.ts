8ab0264ba1b695f75244b4d9f1b17a62ad9a08b7
Mikita Isakau <harley26.gold@gmail.com>
2018-06-24 23:38:04 +0300

    Implement ControlValue Accessor for search component

--- src/app/components/course/course-search/course-search.component.ts
+++ src/app/components/course/course-search/course-search.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-course-search',
   templateUrl: './course-search.component.html',
-  styleUrls: ['./course-search.component.scss']
+  styleUrls: ['./course-search.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CourseSearchComponent),
+      multi: true
+    }
+  ]
 })
-export class CourseSearchComponent implements OnInit {
+export class CourseSearchComponent implements OnInit, ControlValueAccessor {
+
+  public value: string;
+  public disabled = false;
+
+  public onChange = (value: string) => {};
+  public onTouched = () => {};
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
+  onSearch(event: Event): void {
+    console.log('Search', event);
+  }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+    this.onChange(this.value);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 }
