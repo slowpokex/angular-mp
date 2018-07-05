a307aa007d6da5dba216d5a914e76185d205c44e
Mikita Isakau <harley26.gold@gmail.com>
2018-07-05 15:07:43 +0300

    Permit CORS

--- server/main.ts
+++ server/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 import { join } from 'path';
+import config from './config/config';
 
-import './config/config';
-
-const PORT: number = Number.parseInt(process.env.PORT) || 8080;
+const PORT = config.server.port;
 const COMPILED_APP_PATH = '../dist/angular-mp';
+const LOCAL_ASSETS = './assets';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-  app.enableCors();
   app.useStaticAssets(join(__dirname, COMPILED_APP_PATH));
+  app.useStaticAssets(join(__dirname, LOCAL_ASSETS));
   await app.listen(PORT);
 }
 
