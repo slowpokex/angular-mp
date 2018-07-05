9b89a10ee5c49d51b8492aee3bccbfcd1a4bbf48
Mikita Isakau <harley26.gold@gmail.com>
2018-07-05 17:06:56 +0300

    Add draft Graphql support

--- server/app.module.ts
+++ server/app.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { graphqlExpress } from 'apollo-server-express';
+import { GraphQLModule } from '@nestjs/graphql';
 
 import { CommonModule } from './modules/common/common.module';
 import { ApiModule } from './modules/api/api.module';
 
 @Module({
   imports: [
+    GraphQLModule,
     CommonModule,
     ApiModule
   ]
 })
-export class ApplicationModule {}
+export class ApplicationModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(graphqlExpress(req => ({ schema: {}, rootValue: req })))
+      .forRoutes('/graphql');
+  }
+}
--- server/main.ts
+++ server/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 import { join } from 'path';
+import * as express from 'express';
 import config from './config/config';
 
 const PORT = config.server.port;
 const COMPILED_APP_PATH = '../dist/angular-mp';
-const LOCAL_ASSETS = './assets';
+const LOCAL_ASSETS = 'assets/';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
   app.useStaticAssets(join(__dirname, COMPILED_APP_PATH));
-  app.useStaticAssets(join(__dirname, LOCAL_ASSETS));
+  // TODO: Temporary fix
+  app.use('/assets', express.static(join(__dirname, LOCAL_ASSETS)));
   await app.listen(PORT);
 }
 
