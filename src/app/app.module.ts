33adb3ea8f6614188e0f163c2e8be0321efb8028
Mikita Isakau <harley26.gold@gmail.com>
2018-07-29 22:08:35 +0300

    Draft router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourseModule } from './components/course/course.module';
 import { CommonModule } from './components/common/common.module';
-import {LoginModule} from './components/login/login.module';
+import { LoginModule } from './components/login/login.module';
+import { ErrorModule } from './components/error/error.module';
+
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(ROUTES),
     CommonModule,
     LoginModule,
-    CourseModule
+    CourseModule,
+    ErrorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
